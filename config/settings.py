b4d47308e1dd1b0a9e7cf9962ec7adb98a2fad73
grbeno <grbeno@gmail.com>
2025-01-11 18:49:05 +0100

    deployment-1

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','llmchat.up.railway.app']
 
 
 # Application definition
@@ -145,3 +145,5 @@ CHANNEL_LAYERS = {
         },           
      }    
  }
+
+CSRF_TRUSTED_ORIGINS = ['https://llmchat.up.railway.app']
